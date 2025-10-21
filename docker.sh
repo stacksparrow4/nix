2c0d3ea1be76d231e8a78593a3cc43e362388851
Daniel Cooper <daniel.cooper@tantosec.com>
2025-10-21 15:37:50 +1100

    dockerrr

--- docker.sh
+++ docker.sh
@@ -5,11 +5,10 @@ cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null
 
 hmPath=$(nix build .#homeConfigurations.docker.activationPackage --no-link --print-out-paths)
 
-homeMounts=$(ls -A "$hmPath/home-files" | while read line; do
-  echo -n " -v $hmPath/home-files/$line:/root/$line:ro"
+homeMounts=$(find "$hmPath/home-files/" -type l | while read line; do
+echo -n " -v $line:/root/$(realpath -s --relative-to="$hmPath/home-files/" "$line"):ro"
 done)
 
-echo "Home mounts: $homeMounts"
+echo "$homeMounts"
 
 docker run --rm -it -v /nix:/nix:ro -v "$hmPath":/root/.home-manager:ro $homeMounts $(docker build -q .)
-
--- hosts/docker/home/default.nix
+++ hosts/docker/home/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, ... }:
 
 {
   imports = [
@@ -11,8 +11,13 @@
   };
 
   sprrw = {
+    linux.term.enable = true;
     nvim.enable = true;
     programming.enable = true;
+    sec.enable = true;
+    sec.burp.enable = lib.mkForce false;
+    term.enable = true;
+    term.alacritty.enable = lib.mkForce false;
     term.bash.ps1 = ''\n\[\033[1;34m\] \W \$\[\033[0m\] '';
   };
 }
