3a74dd756ca0412351503f68882a2a2d591e5082
Daniel Cooper <daniel.cooper@tantosec.com>
2025-10-21 16:06:36 +1100

    Cleaned up codebase

--- docker.sh
+++ docker.sh
@@ -3,12 +3,14 @@
 set -e
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null
 
+HOME_DIR=/root
+
+# TODO: build this cross platform using docker if we aren't on a NixOS system
+
 hmPath=$(nix build .#homeConfigurations.docker.activationPackage --no-link --print-out-paths)
 
 homeMounts=$(find "$hmPath/home-files/" -type l | while read line; do
-echo -n " -v $line:/root/$(realpath -s --relative-to="$hmPath/home-files/" "$line"):ro"
+echo -n " -v $line:$HOME_DIR/$(realpath -s --relative-to="$hmPath/home-files/" "$line"):ro"
 done)
 
-echo "$homeMounts"
-
-docker run --rm -it -v /nix:/nix:ro -v "$hmPath":/root/.home-manager:ro $homeMounts $(docker build -q .)
+docker run --rm -it -v /nix:/nix:ro -v "$hmPath":"$HOME_DIR/.home-manager":ro $homeMounts $(docker build -q .)
