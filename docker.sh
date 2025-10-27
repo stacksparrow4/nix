0167fad715f4fce3ea4f4f06d6dfcb3f1525826e
Daniel Cooper <daniel.cooper@tantosec.com>
2025-10-28 08:07:26 +1100

    testing

--- docker.sh
+++ docker.sh
@@ -3,24 +3,26 @@
 set -e
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null
 
-HOME_DIR=/root
+export HOME_DIR=/root
 
 if [[ "$(uname -m)" == x86_64 ]] && [[ "$(uname -o)" == "GNU/Linux" ]] && command -v nix >/dev/null; then
-	hmPath=$(nix build .#homeConfigurations.docker.activationPackage --no-link --print-out-paths --show-trace)
+  hmPath=$(nix build .#homeConfigurations.docker.activationPackage --no-link --print-out-paths --show-trace)
 
-	homeMounts=$(find "$hmPath/home-files/" -type l | while read line; do
-		echo -n " -v $line:$HOME_DIR/$(realpath -s --relative-to="$hmPath/home-files/" "$line"):ro"
-	done)
+  homeMounts=$(echo "-v $hmPath:$HOME_DIR/.home-manager:ro"; find "$hmPath/home-files/" -type l | while read line; do
+    echo -n " -v $line:$HOME_DIR/$(realpath -s --relative-to="$hmPath/home-files/" "$line"):ro"
+  done)
 
-	docker run --rm -it -v /nix:/nix:ro -v "$hmPath":"$HOME_DIR/.home-manager":ro $homeMounts $(docker build -q .)
+  docker run --rm -it -v /nix:/nix:ro $homeMounts $(docker build -q .)
 else
-	if ! docker volume inspect nix-store &>/dev/null; then
-		docker volume create nix-store || true
+  if ! docker volume inspect nix-store &>/dev/null; then
+    docker volume create nix-store || true
 
-		docker run --rm --platform linux/amd64 -v nix-store:/nix-original nixos/nix bash -c 'cp -a /nix/* /nix-original'
-	fi
+    docker run --rm --platform linux/amd64 -v nix-store:/nix-original nixos/nix bash -c 'cp -a /nix/* /nix-original'
+  fi
 
-	docker run --rm --platform linux/amd64 -i -v nix-store:/nix -v $(pwd):/pwd:ro nixos/nix bash <<"EOF"
+  tfile=$(mktemp)
+
+  docker run -e HOME_DIR --rm --platform linux/amd64 -i -v "$tfile":/tfile -v nix-store:/nix -v $(pwd):/pwd:ro nixos/nix bash <<"EOF"
 set -e
 mkdir -p ~/.config/nix
 cat <<SECONDEOF > ~/.config/nix/nix.conf
@@ -34,5 +36,13 @@ cd ~/nixos
 hmPath=$(nix build .#homeConfigurations.docker.activationPackage --no-link --print-out-paths --show-trace)
 
 echo "Build complete: $hmPath"
+
+homeMounts=$(echo "-v $hmPath:$HOME_DIR/.home-manager:ro"; find "$hmPath/home-files/" -type l | while read line; do
+  echo -n " -v $line:$HOME_DIR/$(realpath -s --relative-to="$hmPath/home-files/" "$line"):ro"
+done)
+
+echo "$homeMounts" > /tfile
 EOF
+
+  cat "$tfile"
 fi
