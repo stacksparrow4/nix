662e2679753d2561fb3c115289d8495a4a3d3a3b
Daniel Cooper <daniel.cooper@tantosec.com>
2025-11-10 09:26:13 +1100

    Switched to alpine from ubuntu

--- common/home/sandboxing.nix
+++ common/home/sandboxing.nix
@@ -18,10 +18,9 @@
     }:
     let
       dockerFileDir = pkgs.writeTextDir "Dockerfile" ''
-        FROM ubuntu@sha256:66460d557b25769b102175144d538d88219c077c678a49af4afca6fbfc1b5252
+        FROM alpine@sha256:4b7ce07002c69e8f3d704a9c5d6fd3053be500b7f1c69fc0d80990c2ad8dd412
 
-        # Change ubuntu to id 1000 gid 100 and shell bash
-        RUN usermod -g users ubuntu && chsh -s "${pkgs.bash}/bin/bash" ubuntu
+        RUN adduser -s ${pkgs.bash}/bin/bash -G users -D sprrw
       '';
       shareCwdArg = if shareCwd then "-v $(pwd):/pwd" else "";
       dockerinit = import ../../hosts/docker/dockerinit.nix {
