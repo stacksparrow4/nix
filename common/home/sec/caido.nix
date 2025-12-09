d4c59dfbf37b37e33a1108045f79836caa0297e3
Daniel Cooper <daniel.cooper@tantosec.com>
2025-12-10 08:47:31 +1100

    caido upgrade

--- common/home/sec/caido.nix
+++ common/home/sec/caido.nix
@@ -7,16 +7,52 @@
 
   config = let
     cfg = config.sprrw.sec.caido;
-    caidoOverridden = pkgs.caido.override { appVariants = [ "desktop" ]; };
-    caidoDeriv = pkgs.runCommand "caido-wrapper" {} ''
-      mkdir -p $out/share/applications
-      cat ${caidoOverridden}/share/applications/caido.desktop | sed 's/Exec=.*/Exec=caido %U/' > $out/share/applications/caido.desktop
-      ln -s ${caidoOverridden}/share/icons $out/share/icons
-      ln -s ${caidoOverridden}/bin $out/bin
-    '';
+    caido = pkgs.callPackage (
+      {
+        fetchurl,
+        appimageTools,
+        makeWrapper,
+      }:
+      let
+        pname = "caido";
+        version = "0.54.0";
+
+        desktop = fetchurl {
+          url = "https://caido.download/releases/v${version}/caido-desktop-v${version}-linux-x86_64.AppImage";
+          hash = "sha256-akxqz81tetGmzgp9W6Dx4Mi+tYCXpuF70kpQZ6MPu8M=";
+        };
+
+        appimageContents = appimageTools.extractType2 {
+          inherit pname version;
+          src = desktop;
+        };
+
+      in appimageTools.wrapType2 {
+        src = desktop;
+        inherit pname version;
+
+        nativeBuildInputs = [ makeWrapper ];
+
+        extraPkgs = pkgs: [ pkgs.libthai ];
+
+        extraInstallCommands = ''
+          install -m 444 -D ${appimageContents}/caido.desktop -t $out/share/applications
+          install -m 444 -D ${appimageContents}/caido.png \
+            $out/share/icons/hicolor/512x512/apps/caido.png
+          wrapProgram $out/bin/caido \
+            --set WEBKIT_DISABLE_COMPOSITING_MODE 1 \
+            --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}"
+        '';
+      }
+    ) {};
   in lib.mkIf cfg.enable {
     home.packages = [
-      caidoDeriv
+      (pkgs.runCommand "caido-wrapper" {} ''
+       mkdir -p $out/share/applications
+       cat ${caido}/share/applications/caido.desktop | sed 's/Exec=.*/Exec=caido %U/' > $out/share/applications/caido.desktop
+       ln -s ${caido}/share/icons $out/share/icons
+       ln -s ${caido}/bin $out/bin
+       '')
     ];
   };
 }
