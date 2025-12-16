888d67e2776eb56ad2e2fa15b0ead03dcdb7f716
Daniel Cooper <daniel.cooper@tantosec.com>
2025-12-16 13:57:06 +1100

    added vulnx

--- common/home/sec/scanning.nix
+++ common/home/sec/scanning.nix
@@ -5,7 +5,37 @@
     sprrw.sec.scanning.enable = lib.mkEnableOption "scanning";
   };
 
-  config = lib.mkIf config.sprrw.sec.scanning.enable {
+  config = let
+    vulnx = pkgs.callPackage (
+      {
+        buildGoModule,
+        fetchFromGitHub,
+      }:
+
+      buildGoModule {
+        pname = "cvemap";
+        version = "1.0.0";
+
+        src = fetchFromGitHub {
+          owner = "projectdiscovery";
+          repo = "cvemap";
+          rev = "7cda1d46d51d6476eb878e70c2c8deac6ac39a8e";
+          hash = "sha256-dBizKJDVwu5TBNKXAPiLnFXmg5CXJMeLnDX/kguarFg=";
+        };
+
+        vendorHash = "sha256-2Y8alMa0prLL+Id3np+/iuYAZMPQnvR80Rr3LONSSUU=";
+
+        subPackages = [
+          "cmd/vulnx/"
+        ];
+
+        ldflags = [
+          "-s"
+          "-w"
+        ];
+      }
+    ) {};
+  in lib.mkIf config.sprrw.sec.scanning.enable {
     home.packages = with pkgs; [
       nmap
       rustscan
@@ -13,6 +43,7 @@
       sqlmap
       feroxbuster
       ffuf
+      vulnx
     ];
   };
 }
