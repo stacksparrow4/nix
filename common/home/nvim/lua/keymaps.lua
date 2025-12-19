8f07894a291a607d673e704c7c28262ad91243cc
Daniel Cooper <daniel.cooper@tantosec.com>
2025-12-20 10:56:51 +1100

    update

--- common/home/nvim/lua/keymaps.lua
+++ common/home/nvim/lua/keymaps.lua
@@ -24,9 +24,21 @@ vim.keymap.set("n", "<S-TAB>", "<cmd>bprevious<cr>")
 vim.keymap.set("n", "<leader>d", "<cmd>bdelete<cr>")
 vim.keymap.set("n", "<leader>b", "<cmd>BufferLinePick<cr>")
 
+-- Netrw
+vim.keymap.set("n", "<leader>o", "<cmd>Ex<cr>")
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'netrw',
+  callback = function()
+    local buf = vim.api.nvim_get_current_buf()
+    vim.keymap.set('n', 'l', '<CR>', { buffer = buf, remap = true })
+    vim.keymap.set('n', 'h', '-', { buffer = buf, remap = true })
+    vim.keymap.set('n', 'q', ':buffer #<CR>', { buffer = buf, silent = true, nowait = true })
+    vim.keymap.set('n', '<Esc>', ':buffer #<CR>', { buffer = buf, silent = true, nowait = true })
+  end,
+})
+
 -- Misc
 vim.keymap.set("n", "<leader>c", function()
   vim.fn.setreg("+", vim.fn.expand("%:~:.") .. ":" .. vim.fn.line(".") .. ":" .. vim.fn.col("."))
 end, { noremap = true, silent = true, desc = "Copy file path, line, and column" })
 
-vim.keymap.set("n", "<leader>o", "<cmd>Ex<cr>")
