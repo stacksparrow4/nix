cedfb6d417ab010df81b9e0f87822c29eef2577b
Daniel Cooper <daniel.cooper@tantosec.com>
2026-01-08 08:56:44 +1100

    nixpkgs-diff

--- update.sh
+++ update.sh
@@ -5,6 +5,3 @@ cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null
 
 nix flake update
 ./build.sh
-
-# TODO: fix this for home manager only systems
-nix store diff-closures $(echo /nix/var/nix/profiles/system-*-link | grep -oE '[0-9]+' | sort -n | tail -n 2 | while read line; do echo /nix/var/nix/profiles/system-$line-link; done)
