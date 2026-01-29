5a65861870207e7901e97fff34ca947e6f2d6496
Daniel Cooper <daniel.cooper@tantosec.com>
2026-01-29 14:23:36 +1100

    caido docker

--- common/home/sec/caido.nix
+++ common/home/sec/caido.nix
@@ -7,52 +7,21 @@
 
   config = let
     cfg = config.sprrw.sec.caido;
-    caido = pkgs.callPackage (
-      {
-        fetchurl,
-        appimageTools,
-        makeWrapper,
-      }:
-      let
-        pname = "caido";
-        version = "0.55.1";
-
-        desktop = fetchurl {
-          url = "https://caido.download/releases/v${version}/caido-desktop-v${version}-linux-x86_64.AppImage";
-          hash = "sha256-zfts2h8QWTxe/dISwgKRQiSx2nD6vtE1atPfREyGX/U=";
-        };
-
-        appimageContents = appimageTools.extractType2 {
-          inherit pname version;
-          src = desktop;
-        };
-
-      in appimageTools.wrapType2 {
-        src = desktop;
-        inherit pname version;
-
-        nativeBuildInputs = [ makeWrapper ];
-
-        extraPkgs = pkgs: with pkgs; [ libthai chromium ];
-
-        extraInstallCommands = ''
-          install -m 444 -D ${appimageContents}/caido.desktop -t $out/share/applications
-          install -m 444 -D ${appimageContents}/caido.png \
-            $out/share/icons/hicolor/512x512/apps/caido.png
-          wrapProgram $out/bin/caido \
-            --set WEBKIT_DISABLE_COMPOSITING_MODE 1 \
-            --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}"
-        '';
-      }
-    ) {};
   in lib.mkIf cfg.enable {
     home.packages = [
-      (pkgs.runCommand "caido-wrapper" {} ''
-       mkdir -p $out/share/applications
-       cat ${caido}/share/applications/caido.desktop | sed 's/Exec=.*/Exec=caido %U/' > $out/share/applications/caido.desktop
-       ln -s ${caido}/share/icons $out/share/icons
-       ln -s ${caido}/bin $out/bin
-       '')
+      (pkgs.writeShellApplication {
+        name = "caido";
+        text = ''
+        if docker inspect caido &>/dev/null; then
+          echo "Caido already running!"
+          exit 1
+        fi
+
+        mkdir -p ~/.local/share/caido-docker
+        chmod 777 ~/.local/share/caido-docker
+        docker run --rm -d -p 8080:8080 --name caido -v "$HOME/.local/share/caido-docker:/home/caido/.local/share/caido" caido/caido caido-cli --allow-guests --no-renderer-sandbox --listen 0.0.0.0:8080
+        '';
+      })
     ];
   };
 }
