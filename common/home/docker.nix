78d7ab99fb62da0c0fdf47aa274260fc05610f82
Daniel Cooper <stacksparrow4@gmail.com>
2026-02-15 16:21:42 +1100

    removing some legacy stuff

--- common/home/docker.nix
+++ common/home/docker.nix
@@ -1,8 +1,8 @@
 {
-  # # Need to find a way to stop this conflicting with stuff
-  # home.file.".docker/config.json".text = ''
-  #   {
-  #       "detachKeys": "ctrl-z,z"
-  #   }
-  # '';
+  # Need to find a way to stop this conflicting with stuff
+  home.file.".docker/config.json".text = ''
+    {
+        "detachKeys": "ctrl-z,z"
+    }
+  '';
 }
--- hosts/nest01/system/gaming.nix
+++ hosts/nest01/system/gaming.nix
@@ -8,18 +8,18 @@
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
   
-  services.flatpak.enable = true;
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-xapp
-    ];
-  };
-  systemd.services.flatpak-repo = {
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
-  };
+  # services.flatpak.enable = true;
+  # xdg.portal = {
+  #   enable = true;
+  #   extraPortals = with pkgs; [
+  #     xdg-desktop-portal-xapp
+  #   ];
+  # };
+  # systemd.services.flatpak-repo = {
+  #   wantedBy = [ "multi-user.target" ];
+  #   path = [ pkgs.flatpak ];
+  #   script = ''
+  #     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  #   '';
+  # };
 }
