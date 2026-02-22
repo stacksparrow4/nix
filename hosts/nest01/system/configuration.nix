a7b9aab907192570d74c4e9a55cbeadec881f55f
Daniel Cooper <stacksparrow4@gmail.com>
2026-02-22 17:24:39 +1100

    12 hour clock

--- hosts/nest01/system/configuration.nix
+++ hosts/nest01/system/configuration.nix
@@ -76,14 +76,14 @@
     enable = true;
   };
 
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
+  # hardware.nvidia = {
+  #   modesetting.enable = true;
+  #   powerManagement.enable = false;
+  #   powerManagement.finegrained = false;
+  #   open = false;
+  #   nvidiaSettings = true;
+  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # };
 
   # services.xserver.videoDrivers = [ "nvidia" ];
 
