ceff3e575cba92e05135f838c3002f71cece93c0
Daniel Cooper <stacksparrow4@gmail.com>
2026-03-25 21:48:05 +1100

    vm scripts

--- common/home/sandboxing/default.nix
+++ common/home/sandboxing/default.nix
@@ -114,6 +114,40 @@
           docker exec -it "$target" "${dockerInit true}" bash
         '';
       })
+
+      (pkgs.writeShellApplication {
+        name = "build-vm";
+        text = ''
+          cd ~/${config.sprrw.nixosRepoPath}
+          git add .
+
+          isopath=$(nixos-rebuild build-image --flake .#sandbox --image-variant iso --no-link)
+          echo "$isopath"
+
+          mkdir -p ~/.local
+
+          ln -s "$isopath" ~/.local/vm.iso
+        '';
+      })
+      (pkgs.writeShellApplication {
+        name = "vm";
+        text = ''
+          open_port=$(comm -23 <(seq 49152 65535) <(ss -tan | awk '{print $4}' | cut -d':' -f2 | grep "[0-9]\{1,5\}" | sort | uniq) | shuf | head -n 1) || true
+          echo "Forwarding SSH to port $open_port"
+          pidfile=$(mktemp)
+          qemu-system-x86_64 -enable-kvm -m 4096 -cdrom ~/.local/vm.iso -boot d -nic user,hostfwd=tcp:127.0.0.1:"$open_port"-:22 -display none -daemonize -pidfile "$pidfile"
+          qemupid=$(cat "$pidfile")
+          rm "$pidfile"
+          echo "Process id $qemupid"
+
+          sshpass -p password ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o PreferredAuthentications=password localhost -p "$open_port" || true
+
+          echo "Terminating qemu..."
+          kill "$qemupid"
+
+          echo "Done!"
+        '';
+      })
     ];
   };
 }
