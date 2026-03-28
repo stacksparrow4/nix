8e3e010b0b7732540e204966abf764522622ad51
Daniel Cooper <stacksparrow4@gmail.com>
2026-03-28 17:05:14 +1100

    just forward wayland socket

--- common/home/nvim/sandboxed-vim.py
+++ common/home/nvim/sandboxed-vim.py
@@ -3,10 +3,6 @@
 import sys
 import subprocess
 import os
-import socket
-import tempfile
-import threading
-import select
 
 end_bwrap_args_ind = sys.argv.index("ENDBWRAPARGS")
 
@@ -14,135 +10,56 @@ additional_bwrap_args = sys.argv[1:end_bwrap_args_ind]
 vim_path = sys.argv[end_bwrap_args_ind + 1]
 vim_args = sys.argv[end_bwrap_args_ind + 2 :]
 
+additional_vim_args = []
 
-with tempfile.TemporaryDirectory() as tmpdir:
-    finish_r, finish_w = os.pipe()
+proc_args = None
 
-    def copy_thread():
-        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as server:
-            server.bind(f"{tmpdir}/copy.sock")
-            server.listen(1)
+XDG_RUNTIME_DIR = os.getenv("XDG_RUNTIME_DIR")
+assert XDG_RUNTIME_DIR is not None
+WAYLAND_DISPLAY = os.getenv("WAYLAND_DISPLAY")
+assert WAYLAND_DISPLAY is not None
 
-            running = True
+default_bwrap_args = [
+    "bwrap",
+    "--unshare-all",
+    "--as-pid-1",
+    *["--ro-bind", "/nix", "/nix"],
+    *["--ro-bind", "/etc", "/etc"],
+    *["--ro-bind", "/usr", "/usr"],
+    *["--ro-bind", "/run/current-system/sw", "/run/current-system/sw"],
+    *["--ro-bind", "/home/sprrw/.config/nvim", "/home/sprrw/.config/nvim"],
+    *["--tmpfs", "/tmp"],
+    *["--proc", "/proc"],
+    *["--dev", "/dev"],
+    *["--bind", f"{XDG_RUNTIME_DIR}/{WAYLAND_DISPLAY}", f"{XDG_RUNTIME_DIR}/{WAYLAND_DISPLAY}"]
+]
 
-            while running:
-                readable, _, _ = select.select([server, finish_r], [], [])
-
-                if finish_r in readable:
-                    running = False
-                    break
-
-                if server in readable:
-                    conn, _ = server.accept()
-
-                    copy_data = b""
-
-                    with conn:
-                        while running:
-                            readable, _, _ = select.select([conn, finish_r], [], [])
-
-                            if finish_r in readable:
-                                running = False
-                                break
-
-                            data = conn.recv(4096)
-                            if data == b"":
-                                break
-
-                            copy_data += data
-
-                    subprocess.run(["wl-copy"], input=copy_data)
-
-    def paste_thread():
-        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as server:
-            server.bind(f"{tmpdir}/paste.sock")
-            server.listen(1)
-
-            running = True
-
-            while running:
-                readable, _, _ = select.select([server, finish_r], [], [])
-
-                if finish_r in readable:
-                    running = False
-                    break
-
-                if server in readable:
-                    conn, _ = server.accept()
-
-                    data_to_send = subprocess.run(
-                        ["wl-paste", "-n"], stdout=subprocess.PIPE
-                    ).stdout
-
-                    with conn:
-                        while len(data_to_send) != 0:
-                            readable, _, _ = select.select([finish_r], [conn], [])
-
-                            if finish_r in readable:
-                                running = False
-                                break
-
-                            amnt_sent = conn.send(data_to_send)
-
-                            if amnt_sent == 0:
-                                break
-
-                            data_to_send = data_to_send[amnt_sent:]
-
-    t_copy = threading.Thread(target=copy_thread)
-    t_copy.start()
-
-    t_paste = threading.Thread(target=paste_thread)
-    t_paste.start()
-
-    additional_bwrap_args = [*additional_bwrap_args, "--bind", tmpdir, "/tmp/copypaste"]
-    additional_vim_args = [
-        "-c",
-        "lua vim.g.clipboard = { name = 'customClip', copy = { ['+'] = 'socat - UNIX-CONNECT:/tmp/copypaste/copy.sock' }, paste = { ['+'] = 'socat - UNIX-CONNECT:/tmp/copypaste/paste.sock' } }",
-    ]
-
-    proc_args = None
-
-    default_bwrap_args = [
-        "bwrap",
-        "--unshare-all",
-        "--as-pid-1",
-        *["--ro-bind", "/nix", "/nix"],
-        *["--ro-bind", "/etc", "/etc"],
-        *["--ro-bind", "/usr", "/usr"],
-        *["--ro-bind", "/run/current-system/sw", "/run/current-system/sw"],
-        *["--ro-bind", "/home/sprrw/.config/nvim", "/home/sprrw/.config/nvim"],
-        *["--tmpfs", "/tmp"],
-        *["--proc", "/proc"],
-        *["--dev", "/dev"],
-    ]
-
-    share_dir = os.getcwd()
-    if len(vim_args) == 1 and vim_args[0].startswith("/"):
-        arg = vim_args[0]
-        if os.path.isdir(arg):
-            share_dir = arg
-            share_file = "."
-        else:
-            share_dir = os.path.dirname(arg)
-            share_file = os.path.basename(arg)
-
-        additional_vim_args.append(share_file)
+share_dir = os.getcwd()
+if len(vim_args) == 1 and vim_args[0].startswith("/"):
+    arg = vim_args[0]
+    if os.path.isdir(arg):
+        share_dir = arg
+        share_file = "."
     else:
-        additional_vim_args.extend(vim_args)
+        share_dir = os.path.dirname(arg)
+        share_file = os.path.basename(arg)
 
-    args = [
-        *default_bwrap_args,
-        *["--bind", share_dir, share_dir],
-        *additional_bwrap_args,
-        "/usr/bin/env",
-        "PATH=/etc/hm-package/home-path/bin:/run/current-system/sw/bin",
-        vim_path,
-        *additional_vim_args,
-    ]
+    additional_vim_args.append(share_file)
+else:
+    additional_vim_args.extend(vim_args)
 
-    exit_code = subprocess.call(args, cwd=share_dir)
+args = [
+    *default_bwrap_args,
+    *["--bind", share_dir, share_dir],
+    *additional_bwrap_args,
+    "/usr/bin/env",
+    "PATH=/etc/hm-package/home-path/bin:/run/current-system/sw/bin",
+    "XDG_RUNTIME_DIR=" + XDG_RUNTIME_DIR,
+    "WAYLAND_DISPLAY=" + WAYLAND_DISPLAY,
+    vim_path,
+    *additional_vim_args,
+]
 
-    os.write(finish_w, b"\x00")
+exit_code = subprocess.call(args, cwd=share_dir)
 
 exit(exit_code)
