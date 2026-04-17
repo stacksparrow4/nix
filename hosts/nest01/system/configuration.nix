07a104ee4db2a2608b72cc6bc06ff324744baa31
Daniel Cooper <stacksparrow4@gmail.com>
2026-04-17 16:30:19 +1000

    Updated boot loader timeout

--- hosts/nest01/system/configuration.nix
+++ hosts/nest01/system/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, ... }:
+{ config, lib, ... }:
 
 {
   imports = [
@@ -11,6 +11,8 @@
     ../../../common/system
     ./gaming.nix
   ];
+
+  boot.loader.timeout = lib.mkForce 0;
 
   # Swap
   swapDevices = [
