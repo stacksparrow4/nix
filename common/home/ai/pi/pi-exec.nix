7a7686d6e32d89e120660ff13445078212229b4d
Daniel Cooper <daniel.cooper@tantosec.com>
2026-05-25 15:11:44 +1000

    updated pi exec to have execute option

--- common/home/ai/pi/pi-exec.nix
+++ common/home/ai/pi/pi-exec.nix
@@ -28,7 +28,19 @@ pkgs.writeShellApplication {
   text = ''
     output=$(${pi-exec-sandbox}/bin/pi-exec-sandbox --model ${execModel} -p "$@")
     echo "$output"
-    echo -n "$output" | ${pkgs.wl-clipboard}/bin/wl-copy
-    printf "\e[32mCopied to clipboard\e[0m"
+    printf "\n\e[33m[e]\e[0m exec  \e[33m[c]\e[0m copy: "
+    read -r choice
+    case "$choice" in
+      e)
+        eval "$output"
+        ;;
+      c)
+        echo -n "$output" | ${pkgs.wl-clipboard}/bin/wl-copy
+        printf "\e[32mCopied to clipboard\e[0m\n"
+        ;;
+      *)
+        printf "\e[31mAborted\e[0m\n"
+        ;;
+    esac
   '';
 }
