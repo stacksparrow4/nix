c54b1f05a53a7784150d0541c39b63a4804e0185
Daniel Cooper <daniel.cooper@tantosec.com>
2026-05-26 10:08:17 +1000

    Fix

--- common/home/term/yazi.nix
+++ common/home/term/yazi.nix
@@ -10,6 +10,52 @@
   config = lib.mkIf config.sprrw.term.yazi.enable {
     programs.yazi = {
       enable = true;
+      settings = {
+        opener = {
+          viewimg = [
+            {
+              run = "feh $1";
+              orphan = true;
+              for = "unix";
+            }
+          ];
+
+          viewvid = [
+            {
+              run = "vlc $1";
+              orphan = true;
+              for = "unix";
+            }
+          ];
+
+          xdgopen = [
+            {
+              run = "xdg-open $1";
+              orphan = true;
+              for = "unix";
+            }
+          ];
+        };
+
+        open.prepend_rules = [
+          {
+            mime = "image/*";
+            use = "viewimg";
+          }
+          {
+            mime = "video/*";
+            use = "viewvid";
+          }
+          {
+            name = "*.docx";
+            use = "xdgopen";
+          }
+          {
+            name = "*.odt";
+            use = "xdgopen";
+          }
+        ];
+      };
       keymap = {
         mgr.prepend_keymap = [
           {
@@ -25,6 +71,11 @@
             on = [ "D" ];
           }
         ];
+
+        spot.prepend_keymap = [{
+          on = "q";
+          run = "close";
+        }];
       };
     };
   };
