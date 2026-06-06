9b369d9ea96f2ba8f158e1d57cdf2c2a167956d5
Daniel Cooper <daniel.cooper@tantosec.com>
2026-06-07 09:19:37 +1000

    Updated nvim http client

--- common/home/nvim/lua/plugins/httpclient.lua
+++
@@ -1,3 +0,0 @@
-return {
-  { "nvim-http-client", dev = true }
-}
---
+++ common/home/nvim/lua/plugins/nvimhttpclient.lua
@@ -0,0 +1,4 @@
+return {
+  { "nvim-http-client", dev = true }
+  -- { "nvim-http-client", dir = "/home/sprrw/.config/.sprrw-nixos/pkgs/nvim-http-client/" }
+}
