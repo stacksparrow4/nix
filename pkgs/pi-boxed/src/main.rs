dd6011ac7e38fd20fbe19ae6b447cf15851e6c02
Daniel Cooper <daniel.cooper@tantosec.com>
2026-06-19 12:00:23 +1000

    Continuing work on rust pi wrapper

--- pkgs/pi-boxed/src/main.rs
+++ pkgs/pi-boxed/src/main.rs
@@ -1,32 +1,87 @@
-use std::{env, process::{Command, exit}};
+use std::{
+    env,
+    process::{Command, exit},
+};
 
 use clap::Parser;
 
+/// Pi sandbox wrapper. For Pi help, use pi -- --help
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     // Pass through args
+    /// Disable tools by default
+    #[arg(long)]
+    no_tools: bool,
+
+    /// Specify which tools to use
     #[arg(short, long)]
     tools: Option<String>,
 
+    /// Specify models enabled for Ctrl+P cycling
     #[arg(short, long)]
-    prompt: Option<String>,
+    models: Option<String>,
 
-    // Sandbox passthrough options
+    /// Print mode
     #[arg(short, long)]
-    additional_sandbox_args: Option<String>,
+    print: bool,
+
+    // Sandbox arguments
+    /// Pass options directly to the sandbox
+    #[arg(long)]
+    additional_sandbox_args: Vec<String>,
 
     // TODO: add specific passthroughs for common options such as --cwd, --vm, --no-network
 
     // Specific args
+    /// Extensions to enable
     #[arg(short, long)]
     extensions: Option<String>,
 
+    /// System prompt file name to use
     #[arg(short, long)]
     system: Option<String>,
 
+    /// Enable brave search tool and extension
     #[arg(short, long)]
     brave_search: bool,
+
+    /// Positional arguments for Pi
+    args: Vec<String>,
+}
+
+fn get_pi_agent_dir() -> String {
+    format!(
+        "{}/.pi/agent",
+        env::home_dir()
+            .expect("Could not find home directory")
+            .to_string_lossy()
+    )
+}
+
+const BOX_PI_AGENT_DIR: &str = "/home/sprrw/.pi/agent";
+
+enum VolType {
+    File,
+    Dir,
+}
+
+fn generate_pi_volume(host_path: &str, box_path: &str, t: VolType) -> String {
+    format!(
+        "{}/{}:{}/{}:ro:{}",
+        get_pi_agent_dir(),
+        host_path,
+        BOX_PI_AGENT_DIR,
+        box_path,
+        match t {
+            VolType::File => "file",
+            VolType::Dir => "dir",
+        }
+    )
+}
+
+fn generate_mirror_volume(fname: &str, t: VolType) -> String {
+    generate_pi_volume(fname, fname, t)
 }
 
 fn main() {
@@ -47,9 +102,17 @@ fn main() {
 
     let all_tools: Vec<String> = args
         .tools
-        .map_or(vec![], |ts| {
-            ts.split(',').map(|t| t.trim().to_string()).collect()
-        })
+        .map_or(
+            if args.no_tools {
+                vec![]
+            } else {
+                ["read", "write", "edit", "bash"]
+                    .into_iter()
+                    .map(|x| x.to_string())
+                    .collect()
+            },
+            |ts| ts.split(',').map(|t| t.trim().to_string()).collect(),
+        )
         .into_iter()
         .chain(if args.brave_search {
             Some("web_search".to_string())
@@ -58,20 +121,21 @@ fn main() {
         })
         .collect();
 
-    let pi_agent_dir = format!("{}/.pi/agent", env::home_dir().expect("Could not find home directory").to_string_lossy());
-    let box_pi_agent_dir = "/home/sprrw/.pi/agent";
+    let system = args.system.unwrap_or("system-code.md".to_string());
 
     let status = Command::new("sandbox")
         .args([
-            format!("{}/settings.json:{}/sessions:ro:file", pi_agent_dir, box_pi_agent_dir),
-            format!("{}/sessions:{}/sessions:ro:dir", pi_agent_dir, box_pi_agent_dir),
-            // TODO: share models
-            // TODO: SYSTEM.md
-            format!("{}/skills:{}/skills:ro:dir", pi_agent_dir, box_pi_agent_dir),
+            generate_mirror_volume("settings.json", VolType::File),
+            generate_mirror_volume("models.json", VolType::File),
+            generate_mirror_volume("sessions", VolType::Dir),
+            generate_mirror_volume("skills", VolType::Dir),
+            generate_mirror_volume("extensions", VolType::Dir),
+            generate_pi_volume(&system, "SYSTEM.md", VolType::File),
+            generate_mirror_volume("auth.json", VolType::File),
             // TODO: auth.json if network is enabled, unix socket mounting if network is disabled
             // TODO: share brave search config if brave search is enabled
-            // TODO: inject additional_sandbox_args
         ])
+        .args(args.additional_sandbox_args)
         .args([
             "--downgrade-term",
             "--ro-git", // TODO: only use with --cwd
@@ -79,10 +143,26 @@ fn main() {
             "pi",
             "--approve",
             "--no-tools",
-            // TODO: allowlist tools
-            // TODO: allowlist models
-            // TODO inherit additional positional arguments
-        ]).status().expect("Failed to launch sandboxed pi");
+            "--no-extensions",
+        ])
+        .args(if all_tools.len() == 0 {
+            vec![]
+        } else {
+            vec!["--tools".to_string(), all_tools.join(",")]
+        })
+        .args(all_extensions.into_iter().flat_map(|e| {
+            vec![
+                "-e".to_string(),
+                format!("{}/extensions/{}", BOX_PI_AGENT_DIR, e),
+            ]
+        }))
+        .args(
+            args.models
+                .map_or(vec![], |m| vec!["--models".to_string(), m]),
+        )
+        .args(args.args)
+        .status()
+        .expect("Failed to launch sandboxed pi");
 
-    exit(status.code().expect("Could not retrieve status code"));
+    exit(status.code().unwrap_or(1));
 }
