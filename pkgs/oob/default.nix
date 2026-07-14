6615134a8b0174b34c7109c47e70e89145e16cc6
Daniel Cooper <daniel.cooper@tantosec.com>
2026-07-14 19:27:11 +1000

    Fixed OOF

--- pkgs/oob/default.nix
+++ pkgs/oob/default.nix
@@ -8,5 +8,12 @@ pkgs.rustPlatform.buildRustPackage (finalAttrs: {
 
   src = ./.;
 
+  buildInputs = [
+    (pkgs.runCommand "interactsh" { } ''
+      mkdir -p $out/bin
+      ln -s ${import ../interactsh { inherit pkgs; }}/bin/interactsh-client $out/bin/interactsh
+    '')
+  ];
+
   cargoHash = "sha256-yZ+0OJ/Em1hGflTTPu2yVTnbxj8tV9woBkjA+DN5PB0=";
 })
