29b92bb325329fc94feac3700ac1ea84d4af27be
Daniel Cooper <daniel.cooper@tantosec.com>
2026-07-14 19:33:10 +1000

    Fixed double oof

--- pkgs/oob/default.nix
+++ pkgs/oob/default.nix
@@ -2,18 +2,24 @@
   pkgs ? import <nixpkgs-unstable> { },
 }:
 
+let
+  interactsh = pkgs.runCommand "interactsh" { } ''
+    mkdir -p $out/bin
+    ln -s ${import ../interactsh { inherit pkgs; }}/bin/interactsh-client $out/bin/interactsh
+  '';
+in
 pkgs.rustPlatform.buildRustPackage (finalAttrs: {
   pname = "oob";
   version = "0.1.0";
 
   src = ./.;
 
-  buildInputs = [
-    (pkgs.runCommand "interactsh" { } ''
-      mkdir -p $out/bin
-      ln -s ${import ../interactsh { inherit pkgs; }}/bin/interactsh-client $out/bin/interactsh
-    '')
-  ];
+  nativeBuildInputs = [ pkgs.makeWrapper ];
+
+  postInstall = ''
+    wrapProgram $out/bin/oob \
+      --prefix PATH : ${pkgs.lib.makeBinPath [ interactsh ]}
+  '';
 
   cargoHash = "sha256-yZ+0OJ/Em1hGflTTPu2yVTnbxj8tV9woBkjA+DN5PB0=";
 })
