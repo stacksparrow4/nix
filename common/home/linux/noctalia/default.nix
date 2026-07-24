ac9318e59d34673e1dcdefdeb75445826e1a93f9
Daniel Cooper <daniel.cooper@tantosec.com>
2026-07-24 14:38:47 +1000

    Better noctalia screenshot

--- common/home/linux/noctalia/default.nix
+++ common/home/linux/noctalia/default.nix
@@ -17,15 +17,25 @@ pkgs,
     home.file.".config/noctalia/main.toml".source = 
       config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/${config.sprrw.nixosRepoPath}/common/home/linux/noctalia/main.toml";
 
-    home.packages = [(
-      pkgs.writeShellApplication {
+    home.packages = [
+      (pkgs.writeShellApplication {
         name = "noctalia-reset";
         text = ''
           rm -f ~/.local/state/noctalia/settings.toml
           kill "$(pidof noctalia)" || true
           nohup noctalia &>/dev/null &
         '';
-      }
-    )];
+      })
+      (pkgs.writeShellApplication {
+        name = "noctalia-shot-dispatch";
+        runtimeInputs = [ pkgs.wl-clipboard pkgs.swappy pkgs.coreutils ];
+        text = ''
+          case "$(cat /tmp/noctalia-shot-mode 2>/dev/null)" in
+            clipboard) wl-copy --type image/png ;;
+            swappy)    swappy -f - ;;
+          esac
+        '';
+      })
+    ];
   };
 }
