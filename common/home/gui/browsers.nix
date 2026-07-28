8b69161d1a3d39a2090dac0272702597c5c779ff
Daniel Cooper <daniel.cooper@tantosec.com>
2026-07-29 08:41:18 +1000

    Clarified browser mimeapp

--- common/home/gui/browsers.nix
+++ common/home/gui/browsers.nix
@@ -35,5 +35,19 @@
         firefox
         chromium
       ];
+
+      home.sessionVariables.BROWSER = "brave";
+
+      xdg.mimeApps = {
+        enable = true;
+        defaultApplications = lib.genAttrs [
+          "text/html"
+          "application/xhtml+xml"
+          "x-scheme-handler/http"
+          "x-scheme-handler/https"
+          "x-scheme-handler/about"
+          "x-scheme-handler/unknown"
+        ] (_: "brave-browser.desktop");
+      };
     };
 }
