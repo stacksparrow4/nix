fec5e2deaa21eb171d3c8cfa76fafe85b0293daf
Daniel Cooper <daniel.cooper@tantosec.com>
2026-08-17 15:44:56 +1000

    Fixed mistake

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -9,6 +9,8 @@
         (if pkgs.stdenv.hostPlatform.isLinux then self'.packages.nvim else self'.packages.nvim-unboxed)
       ];
 
+      home.sessionVariables.EDITOR = "nvim";
+
       sprrw.term.shellExtra = ''
         # Necessary because of nix path order
         alias vi='nvim'
--- modules/home/term/zshrc.nix
+++ modules/home/term/zshrc.nix
@@ -5,7 +5,6 @@
       home.file.".zshrc".text = ''
         autoload -U colors && colors
         export PS1="%{$fg[blue]%}%1d %{$reset_color%}$ "
-        export EDITOR=nvim
 
         bindkey -e
         bindkey "\e[1;3C" emacs-forward-word
