991b57834c12dca87db131439ed9965ddc7b4deb
Daniel Cooper <daniel.cooper@tantosec.com>
2026-08-20 08:05:02 +1000

    Whoops

--- modules/pkgs/pi/default.nix
+++ modules/pkgs/pi/default.nix
@@ -16,9 +16,9 @@
           pkgs.writeShellApplication {
             name = "pi";
             text = ''
-              export SPRRW_PATH
+              export SPRRW_ADDITIONAL_PATH
               if [[ -d /etc/static/hm-package ]] && [[ -d /run/current-system/sw ]]; then
-                SPRRW_PATH="$(readlink /etc/static/hm-package)/home-path/bin:$(readlink /run/current-system/sw)/bin"
+                SPRRW_ADDITIONAL_PATH="$(readlink /etc/static/hm-package)/home-path/bin:$(readlink /run/current-system/sw)/bin"
               fi
               export SPRRW_PI=${pkgs-unstable.pi-coding-agent}/bin/pi 
 
