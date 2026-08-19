bc8cf5937d47139e439c9e6b0ab88f7cdb789de7
Daniel Cooper <daniel.cooper@tantosec.com>
2026-08-19 13:42:49 +1000

    im dumb

--- modules/pkgs/pi/default.nix
+++ modules/pkgs/pi/default.nix
@@ -17,16 +17,16 @@
             name = "pi";
             text = ''
               export SPRRW_PATH
-              if [[ -d /etc/static/hm-package ]] && [[ -d /run/current-system/sw ]]; then
-                SPRRW_PATH="$(readlink /etc/static/hm-package)/home-path/bin:$(readlink /run/current-system/sw)/bin"
-              fi
+              # if [[ -d /etc/static/hm-package ]] && [[ -d /run/current-system/sw ]]; then
+              #   SPRRW_PATH="$(readlink /etc/static/hm-package)/home-path/bin:$(readlink /run/current-system/sw)/bin"
+              # fi
               export SPRRW_PI=${pkgs-unstable.pi-coding-agent}/bin/pi 
 
               export SPRRW_SKILLS=${./skills}
               export SPRRW_EXTENSIONS=${./extensions}
               export SPRRW_PROMPTS=${./prompts}
 
-              export PATH="${pkgs.lib.makeBinPath [ config.packages.box pkgs.nodejs ]}/bin:$PATH"
+              export PATH="${pkgs.lib.makeBinPath [ config.packages.box pkgs.nodejs ]}:$PATH"
 
               ${pi}/bin/pi "$@"
             '';
