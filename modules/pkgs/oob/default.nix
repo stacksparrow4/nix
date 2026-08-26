82ddbdf5ecfa7f160201aeac6263be829b60b27b
Daniel Cooper <daniel.cooper@tantosec.com>
2026-08-26 13:21:25 +1000

    Fix

--- modules/pkgs/oob/default.nix
+++ modules/pkgs/oob/default.nix
@@ -1,36 +1,62 @@
+{ config, ... }:
+
+let
+  globalConfig = config;
+in
 {
   perSystem =
     {
       pkgs,
+      pkgsLinux,
       config,
       mkSandbox,
       ...
     }:
     {
-      packages = rec {
-        oob-unboxed =
-          let
-            oobBin = (import ./_Cargo.nix { inherit pkgs; }).rootCrate.build;
-          in
-          pkgs.runCommand "oob-unboxed" { nativeBuildInputs = with pkgs; [ makeWrapper ]; meta.mainProgram = "oob"; } ''
-            mkdir -p $out/bin
-            makeWrapper ${oobBin}/bin/oob $out/bin/oob \
-              --prefix PATH : ${pkgs.lib.makeBinPath [ config.packages.interactsh ]}
-          '';
+      packages =
+        let
+          mkOob =
+            { pkgs, interactsh }:
+            let
+              oobBin = (import ./_Cargo.nix { inherit pkgs; }).rootCrate.build;
+            in
+            pkgs.runCommand "oob-unboxed"
+              {
+                nativeBuildInputs = with pkgs; [ makeWrapper ];
+                meta.mainProgram = "oob";
+              }
+              ''
+                mkdir -p $out/bin
+                makeWrapper ${oobBin}/bin/oob $out/bin/oob \
+                  --prefix PATH : ${pkgs.lib.makeBinPath [ interactsh ]}
+              '';
+        in
+        {
+          oob-unboxed = mkOob {
+            inherit pkgs;
+            interactsh = config.packages.interactsh-unboxed;
+          };
 
-        oob = mkSandbox {
-          name = "oob";
-          prog = "${oob-unboxed}/bin/oob";
-          sharedPaths = [
-            {
-              hostPath = "$HOME/.config/interactsh-client/config.yaml";
-              boxPath = "~/.config/interactsh-client/config.yaml";
-              ro = true;
-              type = "file";
-            }
-          ];
-          network = true;
+          oob =
+            let
+              oobWrapped = mkOob {
+                pkgs = pkgsLinux;
+                interactsh = globalConfig.flake.packages.${pkgsLinux.stdenv.hostPlatform.system}.interactsh-unboxed;
+              };
+            in
+            mkSandbox {
+              name = "oob";
+              prog = "${oobWrapped}/bin/oob";
+              sharedPaths = [
+                {
+                  hostPath = "$HOME/.config/interactsh-client/config.yaml";
+                  boxPath = "~/.config/interactsh-client/config.yaml";
+                  ro = true;
+                  type = "file";
+                }
+              ];
+              network = true;
+            };
         };
-      };
     };
 }
