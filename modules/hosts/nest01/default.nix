5ab6b8deaaa1773642946eadfa47e932f4ebe5ab
Daniel Cooper <stacksparrow4@gmail.com>
2026-09-03 17:49:45 +1000

    Added musescore

--- modules/hosts/nest01/default.nix
+++ modules/hosts/nest01/default.nix
@@ -74,39 +74,47 @@ in
                 ai-pi
               ];
 
-              sprrw.ai = {
-                # https://aistudio.google.com/app/api-keys
-                pi.execModel = "gemini-3.6-flash";
+              sprrw = {
+                ai = {
+                  # https://aistudio.google.com/app/api-keys
+                  pi.execModel = "gemini-3.6-flash";
 
-                llama = {
-                  models = [
-                    {
-                      name = "qwen3.5";
-                      path = pkgs.fetchurl {
-                        url = "https://huggingface.co/unsloth/Qwen3.5-9B-GGUF/resolve/main/Qwen3.5-9B-UD-Q3_K_XL.gguf";
-                        hash = "sha256-quCHnhvpnOk/DVYhf4GFo5niWtaKjrvAlfNicGKDBi8=";
-                      };
-                      context = 32768;
-                    }
-                    {
-                      name = "qwen3.6";
-                      path = pkgs.fetchurl {
-                        url = "https://huggingface.co/unsloth/Qwen3.6-35B-A3B-GGUF/resolve/main/Qwen3.6-35B-A3B-UD-Q4_K_M.gguf";
-                        hash = "sha256-rA4sEYngVfqjbv82FYDnnFvW+Odr/7TOVH8WfVPjGmE=";
-                      };
-                      context = 32768;
-                    }
-                    {
-                      name = "qwen3.8";
-                      path = pkgs.fetchurl {
-                        url = "https://huggingface.co/unsloth/Qwen3.8-27B-GGUF/resolve/main/Qwen3.8-27B-UD-Q4_K_M.gguf";
-                        hash = "sha256-Mi4ZT/eXQce6pJfCQPZ39UsgGw76tEyo5Q8SKzkSNII=";
-                      };
-                      context = 32768;
-                      mtp = 2;
-                    }
-                  ];
+                  llama = {
+                    models = [
+                      {
+                        name = "qwen3.5";
+                        path = pkgs.fetchurl {
+                          url = "https://huggingface.co/unsloth/Qwen3.5-9B-GGUF/resolve/main/Qwen3.5-9B-UD-Q3_K_XL.gguf";
+                          hash = "sha256-quCHnhvpnOk/DVYhf4GFo5niWtaKjrvAlfNicGKDBi8=";
+                        };
+                        context = 32768;
+                      }
+                      {
+                        name = "qwen3.6";
+                        path = pkgs.fetchurl {
+                          url = "https://huggingface.co/unsloth/Qwen3.6-35B-A3B-GGUF/resolve/main/Qwen3.6-35B-A3B-UD-Q4_K_M.gguf";
+                          hash = "sha256-rA4sEYngVfqjbv82FYDnnFvW+Odr/7TOVH8WfVPjGmE=";
+                        };
+                        context = 32768;
+                      }
+                      {
+                        name = "qwen3.8";
+                        path = pkgs.fetchurl {
+                          url = "https://huggingface.co/unsloth/Qwen3.8-27B-GGUF/resolve/main/Qwen3.8-27B-UD-Q4_K_M.gguf";
+                          hash = "sha256-Mi4ZT/eXQce6pJfCQPZ39UsgGw76tEyo5Q8SKzkSNII=";
+                        };
+                        context = 32768;
+                        mtp = 2;
+                      }
+                    ];
+                  };
                 };
+
+                flatpaks = [
+                  {
+                    name = "org.musescore.MuseScore";
+                  }
+                ];
               };
 
               home = {
