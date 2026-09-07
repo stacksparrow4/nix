01e4b814ae06ca16b043582fc86e2523164bd1c0
Daniel Cooper <daniel.cooper@tantosec.com>
2026-09-07 15:14:50 +1000

    Cleared final todo

--- TODO.md
+++
@@ -1,1 +0,0 @@
-- Fix fonts in vesktop
--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -1,12 +1,33 @@
 {
   flake.nixosModules.fonts =
     { pkgs, ... }:
-    {
-      fonts.packages = [
-        pkgs.nerd-fonts.iosevka-term
-        pkgs.noto-fonts
-        pkgs.noto-fonts-cjk-sans
-        pkgs.noto-fonts-cjk-serif
+    let
+      fonts = with pkgs; [
+        nerd-fonts.iosevka-term
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
       ];
+    in
+    {
+      fonts.packages = fonts;
+
+      # Bind mounted to /usr/share/fonts for flatpak to use
+      system.fsPackages = [ pkgs.bindfs ];
+      fileSystems."/usr/share/fonts" = {
+        device =
+          pkgs.buildEnv {
+            name = "system-fonts";
+            paths = fonts;
+            pathsToLink = [ "/share/fonts" ];
+          }
+          + "/share/fonts";
+        fsType = "fuse.bindfs";
+        options = [
+          "ro"
+          "resolve-symlinks"
+          "x-gvfs-hide"
+        ];
+      };
     };
 }
