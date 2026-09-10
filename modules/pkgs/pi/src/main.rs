824604dbdabedb88c2d92fb74703241704164825
Daniel Cooper <daniel.cooper@tantosec.com>
2026-09-10 10:06:09 +1000

    Defautl long cache

--- modules/pkgs/pi/src/main.rs
+++ modules/pkgs/pi/src/main.rs
@@ -59,12 +59,12 @@ struct Args {
     models: Option<String>,
 
     /// Disable network, and specify a SOCAT target for the LLM connection
-    #[arg(long)]
+    #[arg(short, long)]
     local: Option<String>,
 
-    /// Use 1 hour (long) Claude cache writes instead of the default 5 minutes
-    #[arg(short = 'l', long)]
-    long_cache: bool,
+    /// Use 5 minute (short) Claude cache writes instead of the default 1 hour
+    #[arg(long)]
+    short_cache: bool,
 
     /// Pass options directly to the sandbox
     #[arg(long)]
@@ -529,10 +529,10 @@ fn main() {
             Target::Remote { .. } => vec![],
             _ => vec!["--env", "PI_READ_AGENTS_MD=1"],
         })
-        .args(if args.long_cache {
-            vec!["--env", "PI_CACHE_RETENTION=long"]
-        } else {
+        .args(if args.short_cache {
             vec![]
+        } else {
+            vec!["--env", "PI_CACHE_RETENTION=long"]
         })
         .args(if brave_search {
             vec![
