aba1fd0fb50c22249179c32f1a47b3bbc74a97ad
Daniel Cooper <stacksparrow4@gmail.com>
2026-09-15 06:47:37 +1000

    Fixed splits

--- modules/home/term/tmux/default.nix
+++ modules/home/term/tmux/default.nix
@@ -17,12 +17,23 @@
 
         extraConfig =
           builtins.replaceStrings
-            [ "REPLACE_WITH_DEFAULT_TERM" "REPLACE_WITH_PANE_CWD_CMD" ]
+            [ "REPLACE_WITH_DEFAULT_TERM" "REPLACE_WITH_SPLITS" ]
             [
               config.sprrw.term.tmux.defaultTerm
               (
 
-                if pkgs.stdenv.isDarwin then "#{pane_current_path}" else "$(readlink /proc/#{pane_pid}/cwd)"
+                if pkgs.stdenv.isDarwin then
+                  ''
+                    bind c new-window -c "#{pane_current_path}"
+                    bind s split-window -c "#{pane_current_path}"
+                    bind a split-window -h -c "#{pane_current_path}"
+                  ''
+                else
+                  ''
+                    bind c run-shell 'tmux new-window -c "$(readlink /proc/#{pane_pid}/cwd)"'
+                    bind s run-shell 'tmux split-window -c "$(readlink /proc/#{pane_pid}/cwd)"'
+                    bind a run-shell 'tmux split-window -h -c "$(readlink /proc/#{pane_pid}/cwd)"'
+                  ''
               )
             ]
             (builtins.readFile ./tmux.conf);
