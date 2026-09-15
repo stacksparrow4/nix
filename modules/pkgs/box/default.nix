35c559cc6c6c851c30d046cd5a3b8f6a40a53170
Daniel Cooper <daniel.cooper@tantosec.com>
2026-09-15 14:12:17 +1000

    Added awk and sed

--- modules/pkgs/box/default.nix
+++ modules/pkgs/box/default.nix
@@ -123,6 +123,8 @@
                   git
                   findutils
                   gnugrep
+                  gnused
+                  gawk
                   ripgrep
                   brave-search-cli
                 ]
